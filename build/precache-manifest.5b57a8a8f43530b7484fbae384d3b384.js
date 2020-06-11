89f47545d123f5b1735cbe3b796bf7812783e2f2
Daniel W <danieldwalford@gmail.com>
2020-06-11 15:52:22 +0100

    build for github pages

---
+++ build/precache-manifest.5b57a8a8f43530b7484fbae384d3b384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6571ebe263dfe228017e33c00c3acdb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6360175c2b3fa16dabe7",
+    "url": "/static/css/main.55141fdd.chunk.css"
+  },
+  {
+    "revision": "54a42299344cbd83325a",
+    "url": "/static/js/2.a3a57a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3a57a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6360175c2b3fa16dabe7",
+    "url": "/static/js/main.a1b2915b.chunk.js"
+  },
+  {
+    "revision": "3a69b3624976add1b012",
+    "url": "/static/js/runtime-main.2e984d3d.js"
+  },
+  {
+    "revision": "3edebd2f7ad44d1a150104c1dc2dd551",
+    "url": "/static/media/Census2021_whiteback.3edebd2f.png"
+  }
+]);
