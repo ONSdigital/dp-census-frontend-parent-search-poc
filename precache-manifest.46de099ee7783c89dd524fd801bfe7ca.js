4029415a8a42b31bd3bc8121f2ce58d5c64e1458
Daniel Walford <danieldwalford@gmail.com>
2020-06-17 12:32:33 +0100

    Updates

---
+++ precache-manifest.46de099ee7783c89dd524fd801bfe7ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82d562eadc552383bc622f939c6630e",
+    "url": "/dp-census-frontend-parent-search-poc/index.html"
+  },
+  {
+    "revision": "bb78d4c2a7327ec4e84b",
+    "url": "/dp-census-frontend-parent-search-poc/static/css/main.8d595ca2.chunk.css"
+  },
+  {
+    "revision": "b0e2f5a866a1bbdd8cd7",
+    "url": "/dp-census-frontend-parent-search-poc/static/js/2.842a5a7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dp-census-frontend-parent-search-poc/static/js/2.842a5a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb78d4c2a7327ec4e84b",
+    "url": "/dp-census-frontend-parent-search-poc/static/js/main.19f13fe9.chunk.js"
+  },
+  {
+    "revision": "3a12863be28cda2e04d1",
+    "url": "/dp-census-frontend-parent-search-poc/static/js/runtime-main.ac0951d7.js"
+  },
+  {
+    "revision": "3edebd2f7ad44d1a150104c1dc2dd551",
+    "url": "/dp-census-frontend-parent-search-poc/static/media/Census2021_whiteback.3edebd2f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0702a8b5387755743a1d9307850e3c14.js"
+  "/dp-census-frontend-parent-search-poc/precache-manifest.46de099ee7783c89dd524fd801bfe7ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dp-census-frontend-parent-search-poc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
