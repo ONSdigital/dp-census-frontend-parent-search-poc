ca2d8e71102fce4577f166c720d32693ab9a3985
Daniel W <danieldwalford@gmail.com>
2020-06-17 12:26:02 +0100

    Start of using openlayer map vanilla

---
+++ build/precache-manifest.1e0d81d4d7d1902560702ef530a95eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb70a4091a379bf160ee8ef04cd6440e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be62e1798ae60538f1a1",
+    "url": "/static/css/main.8d595ca2.chunk.css"
+  },
+  {
+    "revision": "b0e2f5a866a1bbdd8cd7",
+    "url": "/static/js/2.842a5a7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.842a5a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be62e1798ae60538f1a1",
+    "url": "/static/js/main.f4e05681.chunk.js"
+  },
+  {
+    "revision": "3a69b3624976add1b012",
+    "url": "/static/js/runtime-main.2e984d3d.js"
+  },
+  {
+    "revision": "3edebd2f7ad44d1a150104c1dc2dd551",
+    "url": "/static/media/Census2021_whiteback.3edebd2f.png"
+  }
+]);
--- src/Content.tsx
+++ src/Content.tsx
@@ -1,5 +1,7 @@
 import React, {FormEvent} from 'react';
 import {Resulta, ResultProps, searchResult} from "./Result";
+import {OLMapFragment} from './OLMapFragment'
+
 
 import './App.css';
 
@@ -86,6 +88,7 @@ export class Content extends React.Component<ContentProps, {}> {
                     <span className="icon icon-search--light"></span>
                 </button>
             </form>
+            <OLMapFragment/>
             <Resulta results={this.state.results}/>
         </div>
     }
